11ad3fdb466d6f0f2ec3ddc5662a781afc8a0037
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-10 19:19:30 -0800

    Add the store

---
+++ test/store_spec.js
@@ -0,0 +1,28 @@
+import {Map, fromJS} from 'immutable';
+import {expect} from 'chai';
+
+import makeStore from '../src/store';
+
+describe('store', () => {
+
+  it('is a Redux store configured with the correct reducer', () => {
+    const store = makeStore();
+    expect(store.getState()).to.equal(Map());
+
+    store.dispatch({
+      type: 'SET_ENTRIES',
+      entries: ['Trainspotting', '28 Days Later']
+    });
+    expect(store.getState()).to.equal(fromJS({
+      entries: ['Trainspotting', '28 Days Later']
+    }));
+  });
+
+});
+
+/* A Redux Store is an object that stores the state of the
+application over time. It is initialized with a reducer function;
+then you can dispatch actions to that Store. The Store will
+internally use your reducer to apply the actions to the current
+state, and store the resulting, next state.
+*/
