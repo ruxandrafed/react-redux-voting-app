ae16d57d2fea89d50285062196d8394647d99e27
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-10 18:58:48 -0800

    The reducer has an initial state

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,6 @@
-import {setEntries, next, vote} from './core';
+import {setEntries, next, vote, INITIAL_STATE} from './core';
 
-export default function reducer(state, action) {
+export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case 'SET_ENTRIES':
       return setEntries(state, action.entries);
@@ -11,3 +11,10 @@ export default function reducer(state, action) {
   }
   return state;
 }
+
+/* What is interesting about the way this reducer works is how
+it can be generically used to take the application from one state
+to the next, given any type of action. Actually, given a collection
+of past actions, you can actually just reduce that collection into
+the current state. That's why the function is called a reducer:
+It fulfills the contract of a reduce callback function. */
