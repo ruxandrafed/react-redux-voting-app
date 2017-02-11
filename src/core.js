92832cf883fbf2344da498e780c19e6d31674dfb
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-10 17:55:49 -0800

    Add core functionality (setEntries and next) + tests

---
+++ src/core.js
@@ -0,0 +1,17 @@
+import {List, Map} from 'immutable';
+
+export function setEntriesOriginal(state, entries) {
+  return state.set('entries', entries);
+}
+
+export function setEntries(state, entries) {
+  return state.set('entries', List(entries));
+}
+
+export function next(state) {
+  const entries = state.get('entries');
+  return state.merge({
+    vote: Map({pair: entries.take(2)}),
+    entries: entries.skip(2)
+  });
+}
