7e5c59d26babc2fe6f95b596014c2fb260f66ada
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-10 19:26:18 -0800

    Create socket.io server + http server

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import makeStore from './src/store';
+import startServer from './src/server';
 
 export const store = makeStore();
-
 /* Since we also export the store, we can fire up a Node REPL
 (with e.g. babel-node), require the index.js file and interact
 with the application using the store. */
+
+startServer();
