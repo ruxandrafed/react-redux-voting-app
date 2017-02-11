7eb14d0f20d7c7662509edce38cefc93604b89a5
Ruxandra Fediuc <ruxandra.fediuc@gmail.com>
2017-02-10 18:00:34 -0800

    Add vote function and tests

--- src/core.js
+++ src/core.js
@@ -8,10 +8,44 @@ export function setEntries(state, entries) {
   return state.set('entries', List(entries));
 }
 
+function getWinners(vote) {
+  if (!vote) return [];
+  const [a, b] = vote.get('pair');
+  const aVotes = vote.getIn(['tally', a], 0);
+  const bVotes = vote.getIn(['tally', b], 0);
+  if      (aVotes > bVotes)  return [a];
+  else if (aVotes < bVotes)  return [b];
+  else                       return [a, b];
+}
+
 export function next(state) {
-  const entries = state.get('entries');
-  return state.merge({
-    vote: Map({pair: entries.take(2)}),
-    entries: entries.skip(2)
-  });
+  const entries = state.get('entries')
+                       .concat(getWinners(state.get('vote')));
+  /* It is generally a good idea in these state transformation
+  functions to always morph the old state into the new one instead
+  of building the new state completely from scratch.*/
+  if (entries.size === 1) {
+    return state.remove('vote')
+      .remove('entries')
+      .set('winner', entries.first());
+  } else {
+    return state.merge({
+      vote: Map({pair: entries.take(2)}),
+      entries: entries.skip(2)
+    });
+  }
+}
+
+/* Using updateIn makes this pleasingly succinct. What the code
+expresses is "reach into the nested data structure path
+['vote', 'tally', 'Trainspotting'], and apply this function there.
+If there are keys missing along the path, create new Maps in their
+place. If the value at the end is missing, initialize it with 0". /*
+ */
+export function vote(state, entry) {
+  return state.updateIn(
+    ['vote', 'tally', entry],
+    0,
+    tally => tally + 1
+  );
 }
